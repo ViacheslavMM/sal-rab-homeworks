5ad2ac1271dfc872f20fd221071db8f57cfb367d
Viacheslav <vyacheslav.M.M@yandex.ru>
2023-02-03 00:06:11 +0300

    Задание 2.1, 2.2, 2.3 выполнено

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,14 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+    let shippingSum;
+
+    if (productsSum == 0) alert (shippingSum = 0);
+
+    if (productsSum >= freeShippingMinSum) alert (shippingSum = 0);
+
+    if (productsSum > 0 && productsSum < freeShippingMinSum) alert (shippingSum = shippingPrice);
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -34,6 +42,14 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
+    let discountSum;
+
+    if (productsSum >= discountMinSum){
+        discountSum = discountPart * productsSum / 100;
+    } else {
+        discountSum = 0;
+    }
+
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -50,6 +66,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
+    let totalSum = productsSum;
+    totalSum = totalSum - discountSum;
+
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
@@ -58,6 +77,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
+    totalSum = totalSum + shippingSum;
+
+    //freeShipping = shippingSum == 0 ? true : false;
+
+    freeShipping = shippingSum ==0;
+    
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};

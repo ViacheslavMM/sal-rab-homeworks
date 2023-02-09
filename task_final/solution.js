cec7acb2049cff445067ea656504f90ec182e884
Viacheslav <vyacheslav.M.M@yandex.ru>
2023-02-09 23:43:29 +0300

    Финальное задание сделано

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+  let data = {client: '', order: {}, goods: []};
+  data.client = name + " " + phone;
+  data.order.address = (`ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`);
+  data.order.sum = sum;
 
-    let countOfGoods = goods.length;
+  let qw = {};
+  for (let i = 0; i < goods.length; i++) {
+    qw = {};
+    qw.title = goods[i].title;
+    qw.count = goods[i].count;
+    data.goods.push(qw);
+  }
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+  let jsonData = JSON.stringify({data});
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+  return jsonData;   
 }
